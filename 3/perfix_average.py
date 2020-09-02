76d1080e575c5c5fc639c95ea20892028bdacc35
yaoshunqing <yaoshunqing0106@gmail.com>
2020-09-02 20:59:32 +0800

    new

--- 3/perfix_average.py
+++ 3/perfix_average.py
@@ -4,3 +4,27 @@
 # @File : perfix_average.py 
 # @Software: PyCharm
 
+def prefix_average1(S):
+#   O(n^2)
+    n = len(S)
+    A = [0] * n
+    for i in range(n):
+        total = 0
+        for j in range(i + 1):
+            total += S[j]
+        A[j] = total / (j + 1)
+    return A
+
+print(prefix_average1([1,2,4,4,4]))
+
+def prefix_average2(S):
+#   O(n)
+    n = len(S)
+    A = [0] * n
+    total = 0
+    for i in range(n):
+        total += S[i]
+        A[i] = total / (i + 1)
+    return A
+
+print(prefix_average2([1,2,4,4,4]))
